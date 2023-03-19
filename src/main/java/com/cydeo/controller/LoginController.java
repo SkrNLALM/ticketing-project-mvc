60558158a292eec2c059a405d5be38d281386368
SkrNLALM <cakmakskr@gmail.com>
2023-03-19 21:41:03 +0100

    services and controller created

--- src/main/java/com/cydeo/controller/LoginController.java
+++ src/main/java/com/cydeo/controller/LoginController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = {"/login","/"})
-    public String login(){
+    @RequestMapping(value = {"/login","/"})           //the role of this method is to open login page.//locahost:8080 engaging
+     public String login(){
 
         return "login";
     }
---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/create")   //this is mapping provides navigating from welcome page to open user creator page (means click user create page and open it)
+    public String createUser(Model model){
+        model.addAttribute("user", new UserDTO());
+       // model.addAttribute("roles", new )
+
+
+
+       return "/user/create";
+    }
+
+}
